f514323032f755995ca3b54f8f0c561c1405b215
Maxim Polanov <maxdemented372@gmail.com>
2025-09-29 20:50:27 +0100

    moved | TaskProvider from App to main

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,16 @@
 import { AddTaskForm } from './components/AddTaskForm/AddTaskForm';
 import { Card } from './components/Card/Card';
-import { TasksProvider } from './context/TasksContext';
 
 function App() {
 	return (
-		<TasksProvider>
-			<div className='p-8 flex flex-col items-center justify-center'>
-				<Card
-					title='Task List with Priority'
-					className='w-full'
-				>
-					<AddTaskForm />
-				</Card>
-			</div>
-		</TasksProvider>
+		<div className='p-8 flex flex-col items-center justify-center'>
+			<Card
+				title='Task List with Priority'
+				className='w-full'
+			>
+				<AddTaskForm />
+			</Card>
+		</div>
 	);
 }
 
