d36d9e75813b9dcd2f265b0acc6fb367f0ebb764
Maxim Polanov <maxdemented372@gmail.com>
2025-09-30 07:53:02 +0100

    modified | TasksProvider useMemo on value

--- src/app/providers/TasksProvider.tsx
+++ src/app/providers/TasksProvider.tsx
@@ -3,7 +3,7 @@ import { addTaskAction, removeTaskAction } from '@/features/tasks/model/actions'
 import { tasksReducer } from '@/features/tasks/model/reducers/tasksReducer';
 import type { Task } from '@/features/tasks/model/types';
 import type { ReactNode } from 'react';
-import { useReducer } from 'react';
+import { useMemo, useReducer } from 'react';
 
 interface TasksProviderProps {
 	children: ReactNode;
@@ -15,5 +15,7 @@ export const TasksProvider = ({ children }: TasksProviderProps) => {
 	const addTask = (task: Task) => dispatch(addTaskAction(task));
 	const removeTask = (id: string) => dispatch(removeTaskAction(id));
 
-	return <TasksContext.Provider value={{ tasks, addTask, removeTask }}>{children}</TasksContext.Provider>;
+	const value = useMemo(() => ({ tasks, addTask, removeTask }), [tasks]);
+
+	return <TasksContext.Provider value={value}>{children}</TasksContext.Provider>;
 };
